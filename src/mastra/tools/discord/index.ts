469d31a5c1cc39eec35ef6f78df9bf73f1179f20
1030 <45566778+Hizuki1030@users.noreply.github.com>
2025-03-12 20:31:13 +0900

    update

--- src/mastra/tools/discord/index.ts
+++ src/mastra/tools/discord/index.ts
@@ -1,12 +1,15 @@
 import { getContextTool } from "./get-context-tool";
 import { listChannelsTool } from "./list-channels-tool";
+import { bookingRoomTool } from "./booking-room";
 
 export const discordTools = {
   getContextTool,
   listChannelsTool,
+  bookingRoomTool,
 };
 
 export { 
   getContextTool, 
-  listChannelsTool
+  listChannelsTool,
+  bookingRoomTool
 };
